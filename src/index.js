9a61584900b2869c21a666533e66a22e9f0e8eba
Florian LE HEURTE <f.leheurte@gmail.com>
2017-04-02 23:05:56 +0200

    install react-router v4 and react-router-dom v4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { hashHistory } from 'react-router';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Homepage from './modules/Homepage/Homepage';
 import Velib from './modules/Velib/Velib';
 import Autolib from './modules/Autolib/Autolib';
@@ -8,9 +9,11 @@ import './index.scss';
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="/" component={Homepage} />
-    <Route path="/velib" component={Velib} />
-    <Route path="/autolib" component={Autolib} />
+    <div>
+      <Route exact path="/" component={Homepage} />
+      <Route path="/velib" component={Velib} />
+      <Route path="/autolib" component={Autolib} />
+    </div>
   </Router>,
   document.getElementById('root')
 );
